ae761ecc35b7c417e7319445691984d7a648deb8
Jeong <j294lee@gmail.com>
2024-02-17 17:24:41 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "mlops-for-all", // Usually your GitHub org/user name.
-  projectName: "mlops-for-all.github.io", // Usually your repo name.
+  projectName: "jlcan.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
