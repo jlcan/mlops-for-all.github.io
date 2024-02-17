5df280d98bfc954f700c15f11e17b444cc26baf4
Jeong <j294lee@gmail.com>
2024-02-17 17:23:58 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://mlops-for-all.github.io",
+  url: "https://jlcan.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
